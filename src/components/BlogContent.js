f88bd5639b76b7eff43aa0ca599e132535a3e759
njugudev <njugudev@pop-os.localdomain>
2023-07-18 11:02:36 +0300

    Completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  if (!props.isPublished) {
+    // hide unpublished content
+    // return null means "don't display any DOM elements here"
+    return null;
+  } else {
+    // show published content
+  return (
+    <div id="blog-content">{props.articleText}</div>);
 }
 
+}
 export default BlogContent;
